22420e4e0647c80b215927ebf6d37a3605615d6b
joanne <jc3658@cornell.edu>
2026-05-09 03:13:25 -0400

    fixed tb

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Instantiate the DUT:
+  tt_um_joannec34_teenytpu user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
